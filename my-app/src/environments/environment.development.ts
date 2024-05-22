6836a6493938dcad67732985b56811df3040c871
Votre nom <votre-email@example.com>
2024-05-22 20:44:54 +0200

    Modifications

--- my-app/src/environments/environment.development.ts
+++ my-app/src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: false,
     googleClientId: '324579508407-psc6iit07l83nv1nof9c41goangn42ee.apps.googleusercontent.com',
-    apiUrl: 'http://localhost:3000/api',
+    // apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'https://weather-appli-4e63cbcc320e.herokuapp.com/',
+
 
 };
